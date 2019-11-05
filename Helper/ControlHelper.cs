20f254d563681fb04ddf10d278abf87d61c319fc
sandip124 <sandip.choudhary@crystalsolution.com.np>
2019-11-05 09:41:36 +0545

    feat: add control Helper

---
+++ Helper/ControlHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditX.Helper
+{
+    public class ControlHelper
+    {
+        public static Panel findPanel(Form form, string key)
+        {
+            var panel = form.Controls.Find(key, true).FirstOrDefault() as Panel;
+            try
+            {
+                if (panel == null)
+                {
+                    throw new Exception($"Panel with that key {key} is not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return panel;
+        }
+    }
+}
